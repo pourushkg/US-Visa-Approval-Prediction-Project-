dfca9591316a999168c7bad48a61288f82bb7a6f
pourushkg <pkg.21p10161@mtech.nitdgp.ac.in>
2024-02-25 13:28:27 +0530

    setup file done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US-Visa-Approval-Prediction-Project-",
+    version="0.0.0",
+    author="Pourush",
+    author_email="pkg.21p10161@mtech.nitdgp.ac.in",
+    packages=find_packages()
+)
